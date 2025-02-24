cd790ddd2b0c77ba2c8bbb235573a2b77e75325e
Xian Meg Calma <xianmegcalma23@gmail.com>
2025-02-25 00:49:04 +0800

    initial-commit

---
+++ src/data.tsx
@@ -0,0 +1,23 @@
+export const developerList = [
+    {
+      name: 'Kendrick Lamar',
+      role: 'Rapper',
+      description: 'Kendrick Lamar Duckworth is an American rapper. Regarded as one of the greatest rappers of all time, he was awarded the 2018 Pulitzer Prize for Music, becoming the first musician outside of the classical and jazz genres to receive the honor',
+      url: 'https://www.investopedia.com/thmb/QWfQNFAKm7YY7D17s30PR_WjhRU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-2196837244-cd59229199614d8da69e839d12e30909.jpg',
+      alt: 'Kendrick Lamar Pic.'
+    },
+    {
+      name: 'Kanye West',
+      role: 'Rapper',
+      description: 'Ye is an American rapper and record producer. One of the most prominent figures in hip-hop, he is known for his varying musical style and polarizing cultural and political commentary. After dropping out of college to pursue a career in music, West began producing for regional artists in the Chicago area',
+      url: 'https://variety.com/wp-content/uploads/2022/10/Kanye-West-4.jpg?w=1000&h=563&crop=1',
+      alt: 'Kanye Pic.'
+    },
+    {
+      name: 'Hev Abi',
+      role: 'OPM Rap artist',
+      description: 'Gabriel Abilla, known professionally as Hev Abi, is a Filipino rapper from Quezon City. He gained popularity in 2023 with a string of successful singles, starting with the viral "WELCOME2DTQ". He also helped start the independent record label, Downtown Q Entertainment',
+      url: 'https://images.esquiremag.ph/esquiremagph/images/2024/11/15/hev-abi-2-1731680869.jpg',
+      alt: 'Hev Abi Pic'
+    }
+];
